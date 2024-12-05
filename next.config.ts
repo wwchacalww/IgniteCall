5bf9e7bfb49399bff7defdf86c98b9727c7d86d4
Vicente Cartaxo <juniorcartaxo@gmail.com>
2024-12-05 19:09:43 -0300

    Primeiro commit - Setup e Estrutura

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
 };
 
 export default nextConfig;
